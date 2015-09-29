d093b73f8b521a25f3bee6b1a717649b31343209
Georgios Koloventzos <g.koloventzos@gmail.com>
2015-09-29 17:53:53 -0400

    change the matrices

--- hm1_2.m
+++ hm1_2.m
@@ -12,8 +12,8 @@ xerrT =zeros(l,1);
 allerr = zeros(l,1);
 allerrT = zeros(l,1);
 for i=1:l
-    [err(i),model,errT(i)] = multireg(x(1:xlen/2,:),y(1:ylen/2,:),i,x(xlen/2+1:xlen,:),y(ylen/2+1:ylen,:));
-    [xerr(i),model,xerrT(i)] = multireg(x(xlen/2+1:xlen,:),y(ylen/2+1:ylen,:),i,x(1:xlen/2,:),y(1:ylen/2,:));
+    [err(i),model,errT(i)] = multireg(x(1:xlen/2,:),y(1:ylen/2),i,x(xlen/2+1:xlen,:),y(ylen/2+1:ylen));
+    [xerr(i),model,xerrT(i)] = multireg(x(xlen/2+1:xlen,:),y(ylen/2+1:ylen),i,x(1:xlen/2,:),y(1:ylen/2));
 end
 
 allerr = (err+xerr)./2;
